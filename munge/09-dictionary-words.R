ed91c194f43f0d6431da29238a197facc268af26
Nick Allen <nick@nickallen.org>
2014-10-14 18:02:08 -0400

    Dictionary count for Issue #1 fully working and passing all tests.

--- munge/09-dictionary-words.R
+++ munge/09-dictionary-words.R
@@ -3,9 +3,9 @@
 # in each of the domains
 #
 
-# maintaining our own speller ensures that it is not created and
-# destroyed for each domain.
+# maintaining a single speller ensures that it is not created/destroyed for each domain
 s <- getSpeller ()
 
-domains [, dictionary := count_dictionary_substrings (domain, speller = s) ]
-         #, by = domain]
+domains [, 
+         dictionary := count_dictionary_substrings (domain, speller = s), 
+         by = domain]
